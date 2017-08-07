47714d98b3c0b42def29ab0506aaf9ad1e3eb4b9
yyyydevtttt <yyyydevtttt@gmail.com>
2017-08-07 23:11:13 +0900

    issue Trackerの作成
    issue を登録するページを作成した

--- src/app/pages/issue/issue.component.ts
+++ src/app/pages/issue/issue.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Issue } from './issue';
+import { IssueService } from './issue.service';
 
 @Component({
   selector: 'app-issue',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssueComponent implements OnInit {
 
-  constructor() { }
+  issue: Issue;
+  issues: Issue[];
+
+  constructor() {}
 
   ngOnInit() {
+    this.issue = new Issue;
+    this.issues = []
+  }
+
+  public onSubmit(form: NgForm): void {
+    const issue = {
+      title: form.value.title,
+      desc: form.value.desc
+    }
+    this.issues.push(issue);
+    form.reset;
+  }
+
+  public onDelete(index: number): void {
+    this.issues.splice(index, 1);
   }
 
 }
